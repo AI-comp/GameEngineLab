73b72d5aa26524f1a9cabeedff6218fa4f1e0e51
ss0201 <ss0201.777@gmail.com>
2014-03-20 16:41:31 +0900

    Add command center

--- game.js
+++ game.js
@@ -3,15 +3,52 @@
 
 Players = new Meteor.Collection("players");
 Messages = new Meteor.Collection("messages");
+Commands = new Meteor.Collection("commands");
+Games = new Meteor.Collection("games");
+
+Player = function (name) {
+  var self = this;
+  self.name = name;
+  self.ready = false;
+  self.command = "";
+}
+
+Command = function (playerId, text, turn) {
+  var self = this;
+  self.playerId = playerId;
+  self.text = text;
+  self.turn = turn;
+}
+
+Game = function () {
+  var self = this;
+  self.turn = 0;
+}
 
 if (Meteor.isClient) {
-  Template.chat.me = function () {
+  Template.chat.me = Template.command_center.me = function () {
     return Players.findOne(Session.get("me"));
   };
 
-  Template.chat.players = function () {
-    return Players.find({}, {sort: {id: 1, name: 1}});
+  Template.command_center.players = function () {
+    return Players.find({}, { sort: { id: 1, name: 1 } });
   };
+
+  Template.command_center.myCommand = function () {
+    var me = Template.command_center.me();
+    var game = Games.findOne({});
+    return Commands.findOne({ playerId: me._id, turn: game.turn });
+  };
+
+  Template.command_center.events({
+    'click #send_cmd': function () {
+      var me = Template.command_center.me();
+      var commandText = $("#cmd_text").val();
+      var game = Games.findOne({});
+      var command = new Command(me._id, commandText, game.turn);
+      Meteor.call("sendCommand", command);
+    }
+  });
 
   Template.chat.messages = function () {
     return Messages.find({});
@@ -26,7 +63,7 @@ if (Meteor.isClient) {
       Session.set("me", myId);
     },
     'click #ready': function () {
-      Players.update(Session.get("me"), {$set: {ready: true}});
+      Players.update(Session.get("me"), { $set: { ready: true } });
     },
     'click #clear': function () {
       Meteor.call('clear');
@@ -39,16 +76,32 @@ if (Meteor.isClient) {
       });
     }
   });
+
+  Template.player.command = function () {
+    var game = Games.findOne({});
+    if (!game) return undefined;
+    var c = Commands.findOne({ playerId: this._id, turn: game.turn - 1 });
+    console.log(c);
+    return c;
+  };
 }
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Games.insert(new Game());
+
     return Meteor.methods({
-      clear: function() {
+      clear: function () {
         Players.remove({});
         Messages.remove({});
-      }
+        Commands.remove({});
+        Games.remove({});
+      },
+
+      sendCommand: function (command) {
+        Commands.update({ _id: command.id, turn: command.turn }, command, { upsert: true });
+      },
     });
   });
 }
